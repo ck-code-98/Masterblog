a1e62285c3f8453929f35f78d6af051228e2a78d
ck-code-98 <constantin-knappe@web.de>
2025-08-30 15:19:15 +0200

    Created first route and basic data structure

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
+with open ("data.json", encoding="utf-8") as file:
+    data = json.load(file)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = []
+    for element in data:
+        blog_posts.append(element["content"])
+    return render_template('index.html', posts=blog_posts)
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
